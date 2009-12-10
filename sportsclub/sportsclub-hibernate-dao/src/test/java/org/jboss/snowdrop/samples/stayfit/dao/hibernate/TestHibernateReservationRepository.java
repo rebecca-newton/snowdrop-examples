ad55aadf7928f3c6d9e11447035905ece338de67
Lukáš Vlček <lukas.vlcek@gmail.com>
2009-12-10 10:24:14 +0000

    JSF converter now managed by Spring

--- sportsclub/sportsclub-hibernate-dao/src/test/java/org/jboss/snowdrop/samples/stayfit/dao/hibernate/TestHibernateReservationRepository.java
+++ sportsclub/sportsclub-hibernate-dao/src/test/java/org/jboss/snowdrop/samples/stayfit/dao/hibernate/TestHibernateReservationRepository.java
@@ -14,8 +14,6 @@ import org.junit.runner.RunWith;
 
 import java.util.*;
 
-/**
- */
 @ContextConfiguration(locations = {"classpath:test-infrastructure.xml", "classpath:dao-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestHibernateReservationRepository
@@ -25,11 +23,11 @@ public class TestHibernateReservationRepository
 
    @Test
    @Transactional
-   public void testReservationRepository()
+   public void testFromToFilterCriteria()
    {
 
-      Date from = getDate(2009,1,1);
-      Date to = getDate(2009,12,31);
+      Date from = getDate(2009, 1, 1);
+      Date to = getDate(2009, 12, 31);
 
       ReservationSearchCriteria criteria = new ReservationSearchCriteria();
       criteria.setFromDate(from);
@@ -39,35 +37,42 @@ public class TestHibernateReservationRepository
       Assert.assertEquals(6, reservations.size());
 
       criteria = new ReservationSearchCriteria();
-      criteria.setFromDate(getDate(2009,02,01));
+      criteria.setFromDate(getDate(2009, 02, 01));
 
       reservations = reservationRepository.getByCriteria(criteria);
       Assert.assertEquals(5, reservations.size());
 
       criteria = new ReservationSearchCriteria();
-      criteria.setToDate(getDate(2009,11,30));
+      criteria.setToDate(getDate(2009, 11, 30));
 
       reservations = reservationRepository.getByCriteria(criteria);
       Assert.assertEquals(5, reservations.size());
+   }
 
-      
+   @Test
+   @Transactional
+   public void testEquipmentTypeFilterCriteria()
+   {
+      Date from = getDate(2009, 1, 1);
+      Date to = getDate(2009, 12, 31);
+
       List<EquipmentType> types = new ArrayList<EquipmentType>();
       types.add(EquipmentType.COURT);
-      criteria = new ReservationSearchCriteria();
+      ReservationSearchCriteria criteria = new ReservationSearchCriteria();
       criteria.setFromDate(from);
       criteria.setToDate(to);
       criteria.setEquipmentType(types);
 
-      reservations = reservationRepository.getByCriteria(criteria);
+      List<Reservation> reservations = reservationRepository.getByCriteria(criteria);
+
       Assert.assertEquals(1, reservations.size());
-      
    }
 
    private Date getDate(int year, int month, int day)
    {
       Calendar cal = Calendar.getInstance(Locale.US);
       cal.clear();
-      cal.set(year, month-1, day);
+      cal.set(year, month - 1, day);
       return cal.getTime();
    }
 }
