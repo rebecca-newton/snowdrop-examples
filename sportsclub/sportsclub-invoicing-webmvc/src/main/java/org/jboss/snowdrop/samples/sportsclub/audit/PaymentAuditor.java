970530c8fbaa485b8128c2ae39c06ee0dcb29747
Marius Bogoevici <marius.bogoevici@gmail.com>
2010-01-15 00:44:53 +0000

    basic auditing + exposing audit bean w/JMX

---
+++ sportsclub/sportsclub-invoicing-webmvc/src/main/java/org/jboss/snowdrop/samples/sportsclub/audit/PaymentAuditor.java
@@ -0,0 +1,37 @@
+package org.jboss.snowdrop.samples.sportsclub.audit;
+
+import java.math.BigDecimal;
+
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marius Bogoevici
+ */
+@ManagedResource(objectName = "sportsclub:name=paymentAuditor", description = "Payment Auditor", logFile = "log-sportsclub-auditor-jmx.log")
+@Component
+public class PaymentAuditor
+{
+   private boolean enabled = true;
+
+   @ManagedAttribute
+   public void setEnabled(boolean enabled)
+   {
+      this.enabled = enabled;
+   }
+
+   @ManagedAttribute
+   public boolean getEnabled()
+   {
+      return this.enabled;
+   }
+
+   public void auditPayment(Long accountId, BigDecimal amount)
+   {
+      if (this.enabled)
+      {
+         System.out.println("A payment has been made to account " + accountId + " in total of " + amount);
+      }
+   }
+}
